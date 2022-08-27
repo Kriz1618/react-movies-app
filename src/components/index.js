8906d1686598fded954f1004babd66a06c2c8d3c
Christian <krizmarles@gmail.com>
2022-08-26 23:35:38 -0500

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { AddFavorites } from './AddFavorites';
+export { MovieList } from './MovieList';
+export { MovieListHeading } from './MoviListHeading';
+export { SearchBox } from './SearchBox';
+export { RemoveFavorites } from './RemoveFavorites';
